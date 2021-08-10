a47bbc3d0cc9bc2642153c058d9dd5a19672ed7e
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-08-10 12:33:47 +0300

    spec. Entry Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,3 +8,13 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, caption):
+    caption = caption.upper()
+    entry_body = util.get_entry(caption)
+    if entry_body:
+        return render(request, "encyclopedia/entry.html", {
+            "caption": caption,
+            "entry_body": entry_body,
+        })
+    else:
+        return render(request, "encyclopedia/not_found.html")
