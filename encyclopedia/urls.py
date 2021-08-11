34157a938036e742143bc2e9a3a83c1b80fe4b4c
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-08-11 12:45:14 +0300

    spec. Index Page & Search

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:caption>", views.entry, name="entry")
+    path("wiki/<str:caption>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,11 +1,21 @@
-from django.shortcuts import render
+from re import S
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from . import util
+from django import forms
 
+# class SearchForm(forms.Form):
+#     query = forms.CharField(
+#         widget=forms.TextInput(attrs={'placeholder': 'Search'})
+    # )
 
 def index(request):
+    # form = SearchForm()
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(),
+        # "form": form
     })
 
 def entry(request, caption):
@@ -18,3 +28,21 @@ def entry(request, caption):
         })
     else:
         return render(request, "encyclopedia/not_found.html")
+
+
+def search(request):
+    entries  = util.list_entries()
+    entries = [entry.lower() for entry in entries]
+    q = (request.POST.get('q')).lower()
+
+    if q in entries:
+        return HttpResponseRedirect(reverse("entry", kwargs={'caption': q}))
+    else: 
+        search_res = []
+        for entry in entries:
+            if q in entry:
+                search_res.append(entry)
+        return render(request, "encyclopedia/search.html", {
+            "q": q,
+            "search_res": search_res,
+        })
