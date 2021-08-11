ccedd73acc7ae2d43263672c678ee75be7fa6fbf
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-08-11 13:51:32 +0300

    New Page

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,8 @@ from re import S
 from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from . import util
 from django import forms
@@ -46,3 +48,33 @@ def search(request):
             "q": q,
             "search_res": search_res,
         })
+
+
+def validate_caption(value):
+    if util.get_entry(value):
+        raise ValidationError(
+            _('%(value)s already exists.'),
+            params={'value': value},
+        )
+
+def to_h1(caption):
+    return f"# {caption}\n\n"
+ 
+class NewPageForm(forms.Form):
+    caption = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Caption'}),
+    validators=[validate_caption])
+    body = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Body'}))
+def new_page(request):
+    if request.method == "POST":
+        form = NewPageForm(request.POST)
+        if form.is_valid():
+            caption = form.cleaned_data["caption"]
+            body = to_h1(caption) + form.cleaned_data["body"]
+            util.save_entry(caption, body)
+            return HttpResponseRedirect(reverse("entry", kwargs={'caption': caption}))
+        return render(request, "encyclopedia/new_page.html", {
+            "form": form,
+        })
+    return render(request, "encyclopedia/new_page.html",{
+        "form": NewPageForm(),
+    })
